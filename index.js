e2b895591e742871478511365ff0a0419183e40a
Simon Williams <simon.williams@lexisnexis.com>
2014-09-03 16:50:37 +0100

    clean up

--- index.js
+++ index.js
@@ -17,16 +17,3 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
-var pg = require('pg');
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
-    });
-  });
-})
